86500288251be053e0629b3209d00cc46a7a6c2d
Muhammet Ali <94957965+MuhammetAli54@users.noreply.github.com>
2022-01-10 14:49:33 +0300

    Add project files.

---
+++ WebApplicationMVCTanisma/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationMVCTanisma.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Anasayfa()
+        {
+            return View();
+        }
+        public ActionResult Deneme()
+        {
+            //ViewBag nedir
+            ViewBag.Ad = "Ali";
+            //ViewBag ile ViewData birbirinin aynısıdır
+            //Sadece yazımları farklı
+            //Ekranda ViewBag Ad içinde Headkiller'ı göreceğiz
+            //Ali'yi göremiyoruz çünkü 43. satırda ViewBag'in taşıdğı Ad değişken yenden set edilmiştir.
+
+            //ViewData
+            ViewData["Ad"] = "Headkiller";
+            //TempData
+            TempData["Adiniz"] = "Ali";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplicationMVCTanisma/Controllers/OgrenciController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationMVCTanisma.Models;
+
+namespace WebApplicationMVCTanisma.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        // GET: Ogrenci
+        public ActionResult Listele()
+        {
+         List<Ogrenci> ogrList = Ogrenci.OgrencileriGetir();
+            return View(ogrList);
+        }
+
+        [HttpGet]
+        public ActionResult Ekle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Ekle(Ogrenci ogr)
+        {
+            //Veritabanımız olmadığı için  id'yi kendi atama amaçlı bu satırları yazdık
+            var tumOgrler = Ogrenci.OgrencileriGetir();
+            ogr.Id = tumOgrler.Count + 1;
+
+            //Öğrenciyi ekleyelim
+            Ogrenci.OgrenciListesi.Add(ogr);
+
+            //Ekleme yapıldıktan sonra Listele Action'ına redirect olalım.
+
+            return RedirectToAction("Listele");
+        }
+
+        [HttpGet]
+        public ActionResult Guncelle(int id)
+        {
+            //Gelen id sıfırdan büyükse benim listemde onu bul ve View' e gönder.
+            if (id>0)
+            {
+             Ogrenci bulunanOgr = Ogrenci.OgrenciListesi.FirstOrDefault(x => x.Id == id);
+                return View(bulunanOgr);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Guncelle(Ogrenci yeniogr)
+        {
+            Ogrenci guncellenecekEskiOgr = Ogrenci.OgrenciListesi.FirstOrDefault(x => x.Id == yeniogr.Id);
+            if (guncellenecekEskiOgr!=null)
+            {
+                //Öğrenciyi buldun. Bulduğun bu öğrenciye yeni değerlerini ata
+                guncellenecekEskiOgr.Ad = yeniogr.Ad;
+                guncellenecekEskiOgr.Soyad = yeniogr.Soyad;
+                guncellenecekEskiOgr.DogumTarihi = yeniogr.DogumTarihi;
+            }
+
+            return RedirectToAction("Listele");
+        }
+    }
+}
